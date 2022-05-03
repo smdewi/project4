42feb64190d792899e15b549ce670d2824e21342
smdewi <smdewi2020@gmail.com>
2022-05-03 21:54:05 +0800

    Initial set up

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   development: {
-    username: '<YOUR_USER_NAME>',
+    username: 'smdewi',
     password: null,
-    database: '<YOUR_APP_NAME>_development',
+    database: 'labInventory_development',
     host: '127.0.0.1',
     dialect: 'postgres',
   },
